fc1642bd09d2c25a9d7db84c0b358d18476583fc
Scanialady <Scanialady@978a553d-70b3-44f9-906e-bdbb99548d4d>
2017-02-13 22:32:00 +0000

    dmzx button index 3.2.x update v.1.0.1

--- trunk/dmzx/Button Index/trunk/ext/dmzx/buttonindex/language/de/acp_buttonindex.php
+++ trunk/dmzx/Button Index/trunk/ext/dmzx/buttonindex/language/de/acp_buttonindex.php
@@ -1,8 +1,8 @@
 <?php
 /**
 *
-* @version $Id$
-* @package phpBB Extension - Button index [Deutsch]
+* @version $Id$ 
+* @package phpBB Extension - Button index (deutsch)
 * @copyright (c) 2016 dmzx - http://www.dmzx-web.net
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
@@ -34,29 +34,14 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	//ACP
-	'ACP_BUTTONINDEX_TITLE'				=> 'Button Index',
-	'ACP_BUTTONINDEX_CONFIG'			=> 'Einstellungen',
 	'BUTTONINDEX_ENABLE'				=> 'Aktiviere Button on Index',
 	'BUTTONINDEX_ENABLE_EXPLAIN'		=> 'Aktiviert die Button on Index Extension.',
 	'BUTTONINDEX_PLACEHOLDER'			=> 'http://www.google.com',
 	'BUTTONINDEX_PLACEHOLDER_NAME'		=> 'Buttonname',
-	'BUTTONINDEX_REPOSITORY'			=> 'Setze URL',
+	'BUTTONINDEX_REPOSITORY'			=> 'Set URL',
 	'BUTTONINDEX_REPOSITORY_EXPLAIN'	=> 'Gib die URL und den Buttonnamen ein.<br /> Die URL kann relativ sein, wie etwa <em>viewforum.php?f=2</em>',
 	'BUTTONINDEX_REPOSITORY_TEXT'		=> 'Buttonname',
 	'BUTTONINDEX_MORE_LINKS'			=> 'Button hinzufügen',
-	'LOG_BUTTONINDEX_SAVE'				=> '<strong>Button Index Einstellungen geändert</strong>' ,
 	'BUTTONINDEX_SAVED'					=> 'Button Index Einstellungen gespeichert',
 	'BUTTONINDEX_VERSION'				=> 'Version',
-	'BUTTONINDEX_VERSION_CHECK'			=> 'Button Index Versionsprüfung',
-	'BUTTONINDEX_AUTHOR'				=> 'Autoren',
-	'BUTTONINDEX_ANNOUNCEMENT_TOPIC'	=> 'Release Ankündigung',
-	'BUTTONINDEX_CURRENT_VERSION'		=> 'Derzeitige Version',
-	'BUTTONINDEX_VERSION'				=> 'Version',
-	'BUTTONINDEX_DOWNLOAD_LATEST'		=> 'Neueste Version herunterladen',
-	'BUTTONINDEX_DOWNLOAD'				=> 'Download',
-	'BUTTONINDEX_LATEST_VERSION'		=> 'Neueste Version',
-	'BUTTONINDEX_NOT_UP_TO_DATE'		=> '%s ist nicht aktuell',
-	'BUTTONINDEX_RELEASE_ANNOUNCEMENT'	=> 'Ankündigungsthema',
-	'BUTTONINDEX_UP_TO_DATE'			=> '%s ist aktuell',
 ));
