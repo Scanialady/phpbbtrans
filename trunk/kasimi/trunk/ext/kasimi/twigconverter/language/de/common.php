3f01288d4c955bebbc8430010e76d7704e8e1cd0
Scanialady <Scanialady@978a553d-70b3-44f9-906e-bdbb99548d4d>
2017-07-24 11:21:07 +0000

    kasimi twig converter initial upload - v.1.1.1

---
+++ trunk/kasimi/trunk/ext/kasimi/twigconverter/language/de/common.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ *
+ * Twig Converter. An extension for the phpBB Forum Software package (deutsch).
+ * @version $Id$
+ * @copyright (c) 2017, kasimi, https://kasimi.net
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters for use
+// ‚ ‘ ’ « » „ “ ” …
+
+$lang = array_merge($lang, array(
+	'TWIGCONVERTER_SELECT'						=> 'Konvertiere Template-Dateien',
+	'TWIGCONVERTER_SELECT_EXPLAIN'				=> 'Wähle eine Erweiterung oder einen Style, um die zugehörigen Template-Dateien auf die Twig-Syntax zu konvertieren. Es wird eine zip-Datei mit den neuen Dateien heruntergeladen, und auch im Verzeichnis „/store“ des Boards gespeichert.',
+	'TWIGCONVERTER_SELECT_EXT'					=> 'Wähle Extension oder Style aus',
+	'TWIGCONVERTER_EXT'							=> 'Extension: %s',
+	'TWIGCONVERTER_STYLE'						=> 'Style: %s',
+
+	'TWIGCONVERTER_ERROR_NO_TEMPLATE_FILES'		=> 'Das gewählte Verzeichnis enthält keinerlei Template-Dateien.',
+	'TWIGCONVERTER_ERROR_WRITEABLE'				=> 'Das Zielverzeichnis ist nicht beschreibbar.',
+	'TWIGCONVERTER_ERROR_ZIP'					=> 'Erstellung der zip-Datei fehlgeschlagen.',
+));
